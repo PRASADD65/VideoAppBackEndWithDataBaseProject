8311440f29c0f091d59b00d74d0416d8d71f401c
abhishekklabhi6 <abhishekklabhi7@gmail.com>
2025-03-24 20:07:59 +0530

    added

--- src/main/java/com/example/student/demo/student/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.student.demo.student.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.student.demo.student.module.User;
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByFirstNameAndLastName(String firstName, String lastName);
-    User findByFirstNameAndPassword(String firstName, String password);
-    User findByFirstName(String firstName);
-}
